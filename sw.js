5fe4637de5b23abe08ceb5a5a98c64a710784ab5
vinothvbt <147151138+vinothvbt@users.noreply.github.com>
2024-09-23 18:33:48 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,19 @@
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open('hello-world-pwa').then((cache) => {
-            return cache.addAll([
-                '/',
-                'index.html',
-                'style.css',
-                'manifest.json',
-                'icon-192.png',
-                'icon-512.png'
-            ]);
-        })
+      caches.open('hello-world-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html'
+        ]);
+      })
     );
-});
-
-self.addEventListener('fetch', (event) => {
+  });
+  
+  self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
     );
-});
+  });
+  
